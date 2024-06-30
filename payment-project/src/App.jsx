9657073090feb1eeda9f591bafe741960b204637
Sree2604 <ds04aranganthan@gmail.com>
2024-06-30 13:29:06 +0530

    ...

--- payment-project/src/App.jsx
+++ payment-project/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PaymentForm from "./components/PaymentForm";
 import PaymentStatus from "./components/PaymentStatus";
+
 export default function App() {
   return (
     <Router>
